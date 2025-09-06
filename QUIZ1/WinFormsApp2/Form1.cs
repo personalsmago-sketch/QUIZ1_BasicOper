6bc7c870f96ae628ee4c830643066b71fee04a4e
John Nicole Mago <personalsmago@gmail.com>
2025-09-06 19:01:36 +0800

    Update Form1.cs

--- QUIZ1/WinFormsApp2/Form1.cs
+++ QUIZ1/WinFormsApp2/Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace WinFormsApp2
 {
     public partial class Form1 : Form
@@ -23,8 +25,22 @@ namespace WinFormsApp2
             comboBox1.Items.Add("Bachelor of Science in Mechanical Engineering");
             comboBox1.Items.Add("Bachelor of Engineering Technology");
 
+            textBox12.Enabled = false; // Credit Units
+            textBox13.Enabled = false; // Total Number of Units
+            textBox15.Enabled = false; // Total Tuition Fee
+            textBox16.Enabled = false; // Total Miscellaneous Fee
+            textBox18.Enabled = false; // Total Tuition Fee
+            textBox19.Enabled = false; // Total Miscellaneous Fee
+            textBox20.Enabled = false; // Total Tuition and Fee
+            textBox21.Enabled = false; // Total Number of Units
+            textBox22.Enabled = false; // Cisco Laboratory Fee
+            textBox23.Enabled = false; // Laboratory Fee
+            textBox24.Enabled = false; // Exam Booklet Fee
+            textBox25.Enabled = false; // Total Other Fees
+            textBox27.Enabled = false; // Total Tuition and Fee
+
         }
-        
+
         private void button1_Click(object sender, EventArgs e)
         {
             //code for navigating and uploading image
@@ -55,22 +71,27 @@ namespace WinFormsApp2
         // Submit Button
         private void button2_Click(object sender, EventArgs e)
         {
-            
+
+            double lecture, lab, sum;
+            lecture = Convert.ToDouble(textBox5.Text);
+            lab = Convert.ToDouble(textBox7.Text);
+
+            sum = lecture + lab;
+            textBox12.Text = sum.ToString();    //Credit Units
+
+            listBox1.Items.Add(textBox2.Text); // List Box for Student Name
             listBox2.Items.Add(textBox10.Text); // List Box for Course Code
             listBox3.Items.Add(textBox9.Text); // List Box for Course Desc
             listBox4.Items.Add(textBox5.Text); // List Box for Unit Lecture
             listBox5.Items.Add(textBox7.Text); // List Box for Unit Laboratory
             listBox6.Items.Add(textBox12.Text); // List Box for Credit Units
-            listBox6.Items.Add(textBox8.Text); // List Box for Time 
-            listBox6.Items.Add(textBox11.Text); // List Box for Day 
+            listBox8.Items.Add(textBox8.Text); // List Box for Time 
+            listBox7.Items.Add(textBox11.Text); // List Box for Day 
+  
 
-            
-            textBox18.Text = textBox15.Text; // textBox Total Tuition Fee to Text Box Total Tuition Fee
-            textBox19.Text = textBox16.Text; // textBox Total Miscellaneous Fee to Text Box Total Miscellaneous Fee
-            textBox21.Text = textBox13.Text; // textBox Total Number of Units to Text Box Total Number of Units
-            textBox20.Text = textBox27.Text; // textBox Total Tuition and Fee to Text Box Total Tuition and Fee
-            textBox22.Text = textBox17.Text; // textBox Cisco Lab Fee to Text Box Cisco Laboratory Fee
-            textBox24.Text = textBox26.Text; // textBox Exam Booklet Fee to Text Box Exam Booklet Fee
+
+
+
         }
 
         // # List Box
@@ -98,6 +119,7 @@ namespace WinFormsApp2
 
         private void button3_Click(object sender, EventArgs e)
         {
+
             textBox1.Clear();
             textBox2.Clear();
             textBox3.Clear();
@@ -115,6 +137,56 @@ namespace WinFormsApp2
             textBox15.Clear();
             textBox16.Clear();
             textBox17.Clear();
+            textBox26.Clear();
+            textBox27.Clear();
+
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+         //Calculate Button
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            double lecture, lab, LabFee, CiscoLab, ExamBook, sum, product;
+
+            lecture = Convert.ToDouble(textBox5.Text);
+            lab = Convert.ToDouble(textBox7.Text);
+            LabFee = Convert.ToDouble(textBox14.Text);
+            CiscoLab = Convert.ToDouble(textBox17.Text);
+            ExamBook = Convert.ToDouble(textBox26.Text);
+
+
+            sum = lecture + lab;
+            textBox12.Text = sum.ToString();    //Credit Units
+            textBox13.Text = textBox12.Text;    //Total Number of Units
+
+            product = sum * 1500;
+            textBox15.Text = product.ToString(); //Total Tuition Fee
+
+            sum = LabFee + CiscoLab + ExamBook;
+            textBox16.Text = sum.ToString();    //Total Other Fees
+            textBox25.Text = sum.ToString();    //Total Other Fees
+
+            sum = product + LabFee + CiscoLab + ExamBook;
+            textBox27.Text = sum.ToString();    //Total Tuition and Fee
+            textBox20.Text = sum.ToString();    //Total Tuition and Fee
+
+            textBox18.Text = textBox15.Text; // textBox Total Tuition Fee to Text Box Total Tuition Fee
+            textBox19.Text = textBox16.Text; // textBox Total Miscellaneous Fee to Text Box Total Miscellaneous Fee
+            textBox21.Text = textBox13.Text; // textBox Total Number of Units to Text Box Total Number of Units
+            textBox22.Text = textBox17.Text; // textBox Cisco Lab Fee to Text Box Cisco Laboratory Fee
+            textBox23.Text = textBox14.Text; // textBox Laboratory Fee to Text Box Laboratory Fee
+            textBox24.Text = textBox26.Text; // textBox Exam Booklet Fee to Text Box Exam Booklet Fee
+
+   
+
         }
     }
 }
